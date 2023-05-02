fe6a730a06433ea820b6de86b3c9b8f081371550
red4game <redemgaiming@gmail.com>
2023-05-02 16:02:15 +0200

    Slide show with tmdb

--- app/src/main/java/red/project/uni/lu/ui/home/HomeFragment.java
+++ app/src/main/java/red/project/uni/lu/ui/home/HomeFragment.java
@@ -1,6 +1,7 @@
 package red.project.uni.lu.ui.home;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,31 +11,43 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.denzcoskun.imageslider.ImageSlider;
 import com.denzcoskun.imageslider.constants.ScaleTypes;
 import com.denzcoskun.imageslider.models.SlideModel;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 
+import red.project.uni.lu.BuildConfig;
 import red.project.uni.lu.R;
 import red.project.uni.lu.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    private RequestQueue mRequestQueue;
+    private StringRequest mStringRequest;
+    ArrayList<SlideModel> models;
+    ArrayList<Integer> moviesIDS;
+    ImageSlider imageSlider;
+    View view;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.fragment_home, container, false);
-        ImageSlider imageSlider = view.findViewById(R.id.CinemaSlider);
-        ArrayList<SlideModel> models = new ArrayList<>();
+        view = inflater.inflate(R.layout.fragment_home, container, false);
+        imageSlider = view.findViewById(R.id.CinemaSlider);
+        models = new ArrayList<>();
+        moviesIDS = new ArrayList<>();
 
-        models.add(new SlideModel("https://picsum.photos/200/300", ScaleTypes.FIT));
-        models.add(new SlideModel("https://picsum.photos/300/300", ScaleTypes.FIT));
-        models.add(new SlideModel("https://picsum.photos/300/200", ScaleTypes.FIT));
-        models.add(new SlideModel("https://picsum.photos/400/400", ScaleTypes.FIT));
-        imageSlider.setImageList(models, ScaleTypes.FIT);
+        getMovies();
+
+
 
 
 
@@ -46,4 +59,42 @@ public class HomeFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
+    // Make api call to get the movies from the database and return url of the image + title
+    private void getMovies() {
+
+        String url = "https://api.themoviedb.org/3/movie/upcoming?api_key="+ BuildConfig.TMDB_API_KEY +"&language=fr-FR&page=1&region=FR";
+        mRequestQueue = Volley.newRequestQueue(view.getContext());
+        mStringRequest = new StringRequest(url, response -> {
+            try {
+                System.out.println(response);
+                JSONObject jsonObj = new JSONObject(response);
+                for (int i = 0; i < jsonObj.getJSONArray("results").length(); i++) {
+                    JSONObject movie = jsonObj.getJSONArray("results").getJSONObject(i);
+                    int id = movie.getInt("id");
+                    String title = movie.getString("title");
+                    String date = movie.getString("release_date");
+                    String poster_path = movie.getString("poster_path");
+                    models.add(new SlideModel("https://image.tmdb.org/t/p/w500/" + poster_path, title + " - " + date, ScaleTypes.FIT));
+
+                    // Add new click listener to the image slider
+                    imageSlider.setItemClickListener(position -> {
+                        Bundle bundle = new Bundle();
+                        bundle.putInt("movieID", moviesIDS.get(position));
+                        System.out.println(moviesIDS.get(position));
+                    });
+
+                    moviesIDS.add(id);
+                }
+                imageSlider.setImageList(models, ScaleTypes.FIT);
+            } catch (JSONException e) {
+                throw new RuntimeException(e);
+            }
+
+        }, error -> {
+            Log.i("Error", error.toString());
+        });
+        mRequestQueue.add(mStringRequest);
+    }
+
 }
