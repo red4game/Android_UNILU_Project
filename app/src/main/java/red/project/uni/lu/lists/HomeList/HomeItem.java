490af3832412c7dd8ca4a8f301fea6cb6038463d
red4game <redemgaiming@gmail.com>
2023-05-08 09:19:49 +0200

    home fragment layout

---
+++ app/src/main/java/red/project/uni/lu/lists/HomeList/HomeItem.java
@@ -0,0 +1,59 @@
+package red.project.uni.lu.lists.HomeList;
+
+public class HomeItem {
+    String previewUrl;
+    String title;
+    String description;
+    String dateOfRelease;
+    String Rating;
+
+    public HomeItem(String previewUrl, String title, String description, String dateOfRelease, String Rating) {
+        this.previewUrl = previewUrl;
+        this.title = title;
+        this.description = description;
+        this.dateOfRelease = dateOfRelease;
+        this.Rating = Rating;
+    }
+
+    public String getPreviewUrl() {
+        return previewUrl;
+    }
+
+    public void setPreviewUrl(String previewUrl) {
+        this.previewUrl = previewUrl;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDateOfRelease() {
+        return dateOfRelease;
+    }
+
+    public void setDateOfRelease(String dateOfRelease) {
+        this.dateOfRelease = dateOfRelease;
+    }
+
+    public String getRating() {
+        return Rating;
+    }
+
+    public void setRating(String rating) {
+        Rating = rating;
+    }
+
+
+}
